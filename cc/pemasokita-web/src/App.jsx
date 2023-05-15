0b765c2498bebbba99247f6f5fbd11b4e8dab414
Kalam Mahardhika <kalammahardhika1002@gmail.com>
2023-05-15 20:59:55 +0700

    First commit : Creating folder and landing page web using react js

---
+++ cc/pemasokita-web/src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Header, Overview, ClickToAction, FrequentlyAskedQuestion, Download, Footer } from "./components"
+
+export default function App() {
+  return (
+    <>
+      <Navbar/>
+      <Header/>
+      <Overview/>
+      <ClickToAction/>
+      <FrequentlyAskedQuestion/>
+      <Download/>
+      <Footer/>
+    </>
+  )
+}
---
+++ cc/pemasokita-web/src/components/ClickToAction.jsx
@@ -0,0 +1,9 @@
+export default function ClickToAction() {
+  return (
+    <>
+      <div className="bg-green-200 flex justify-center w-[80%] mx-auto mt-10 p-10 rounded-md">
+        <p className="text-3xl font-medium text-green-600">Click To Action</p>
+      </div>
+    </>
+  )
+}
